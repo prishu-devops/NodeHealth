b9a2232908c977455a82a72ed8e4e9dabd48357b
prishu-devops <devops.prishu@gmail.com>
2025-04-08 22:16:22 +0530

    Create NodeHealth.sh
    This to to check health of the server

---
+++ NodeHealth.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Meta Data of the file
+#########################################
+#Author : Prishu
+#Created on : 8th April 2025
+#
+# THIS TO OUTPUT THE NODE HEALTH
+#########################################
+
+echo "Print the Disk Space"
+df -h
+
+echo "Print the memory"
+free -g
+
+echo "Print the number of CPU"
+nproc
+
+echo "Other way debug mode"
+
+set -x #debug mode
+
+df -h
+free -g
+nproc
